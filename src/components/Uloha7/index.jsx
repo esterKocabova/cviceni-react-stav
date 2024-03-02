ebbf5104370e3f7c159bd4c6ae1eca1570aa3b7a
Ester <kocabova@mamiart.cz>
2024-03-02 22:53:30 +0100

    ukol-7

--- src/components/Uloha7/index.jsx
+++ src/components/Uloha7/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './ukazatel-uspechu.css'
 
 /*
@@ -8,18 +9,20 @@ Zadání 4. Nastav prvku `ukazatel-uspechu__postup` šířku podle stavové prom
 */
 
 const UkazatelPokroku = ({ barva }) => {
+  const [postup, setPostup] = useState(0)
   return (
     <div className="ukazatel-uspechu">
       <div className="ukazatel-uspechu__ramecek">
         <div
           className="ukazatel-uspechu__postup"
           style={{
-            width: '40%',
+            width: `${postup}%`,
             backgroundColor: 'red',
+            barva: {barva}
           }}
         ></div>
       </div>
-      <button>postoupit o 10 %</button>
+      <button onClick={() => setPostup(postup + 10)} disabled={postup === 100}> postoupit o 10 %</button>
     </div>
   )
 }
